7dd0cfdcf30feb0d41f5de680cabc69114109c30
leonardog9823 <guanuco43@gmail.com>
2021-11-22 01:15:55 -0300

    zoom img agregad

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,18 +1,34 @@
 const imgCuadricula = document.querySelectorAll("#cuadricula img");
 const galeria = document.querySelector("#galeria");
+let imgs = [];
+let currentImgUnique = 0;
+let currenSrcImg = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-    imgEvent()
+    imgEvent();
 })
 
 function imgEvent(){
     imgCuadricula.forEach( (img, idx) => {
         img.addEventListener("click", zoomImg)
-        img.dataset.img = idx + 1
+        img.dataset.img = idx + 1;
+        imgs.push(img);
     })
 }
 
 function zoomImg(e){
+
+    const currentImg = document.querySelector("#zoom");
+
+    if(currenSrcImg.length === 0){
+        console.log("Bienvenido")
+        currenSrcImg = [e.target];
+        currentImgUnique = e.target.dataset.img;
+    }
+    if(currentImg){
+        currentImg.remove();
+    }
+    
     const divContainer = document.createElement("div");
     divContainer.classList = "zoom";
     divContainer.id = "zoom";
@@ -24,21 +40,33 @@ function zoomImg(e){
     iconClose.classList = "fas fa-times iconClose";
     
     const img = document.createElement("img");
+    img.dataset.img = currenSrcImg[0].dataset.img;
     img.classList = "zoom--img";
-    img.src = e.target.src;
+    img.src = currenSrcImg[0].src;
     img.alt = "Feria Vanguardia";
 
-    divContainer.appendChild(iconClose)
+    const arrowRight = document.createElement("i");
+    arrowRight.classList = "fas fa-arrow-right iconRight";
+    arrowRight.onclick = rightImg
+
+    divContainer.appendChild(iconClose);
+    divContainer.appendChild(arrowRight);
     imgContainer.appendChild(img);
     divContainer.appendChild(imgContainer);
     galeria.appendChild(divContainer);
 
     iconClose.onclick = () => {
         divContainer.remove();
+        currentImgUnique = 0;
+        currenSrcImg = [];
     };
 }
 
-function iconClose(){
-    const zoom = document.querySelector("#zoom");
-    console.log("cerradp")
+function rightImg(){
+    const imgCurrent = imgs.find( img => Number(img.dataset.img) === (Number(currentImgUnique) + 1));
+    currenSrcImg = [imgCurrent];
+    currentImgUnique = imgCurrent.dataset.img
+    console.log(currenSrcImg)
+
+    zoomImg();
 }
